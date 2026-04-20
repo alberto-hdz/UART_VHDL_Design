04325868b7414e774194836e7fd0be47f85e9c75
alberto-hdz <alberto.hdz1063@gmail.com>
2026-04-19 20:28:29 -0400

    Update tb_ram to test 5 different addresses

--- tb/tb_ram.vhd
+++ tb/tb_ram.vhd
@@ -26,7 +26,6 @@ begin
             dout => dout
         );
 
-    -- clock
     process
     begin
         while true loop
@@ -35,42 +34,52 @@ begin
         end loop;
     end process;
 
-    -- test
     process
     begin
         --------------------------------------------------
-        -- write to address 0
+        -- Write to 5 different addresses
         --------------------------------------------------
-        addr <= "00000";
-        din  <= x"12345678";
-        we   <= '1';
-        wait for CLK_PERIOD;
-
+        addr <= "00000"; din <= x"11111111"; we <= '1'; wait for CLK_PERIOD;
+        addr <= "00001"; din <= x"22222222";              wait for CLK_PERIOD;
+        addr <= "00010"; din <= x"33333333";              wait for CLK_PERIOD;
+        addr <= "00011"; din <= x"44444444";              wait for CLK_PERIOD;
+        addr <= "00100"; din <= x"55555555";              wait for CLK_PERIOD;
         we <= '0';
         wait for CLK_PERIOD;
 
         --------------------------------------------------
-        -- read address 0
+        -- Read back and verify all 5 addresses
         --------------------------------------------------
+        addr <= "00000"; wait for CLK_PERIOD;
+        assert dout = x"11111111"
+            report "ERROR: addr 0 read failed" severity error;
+
+        addr <= "00001"; wait for CLK_PERIOD;
+        assert dout = x"22222222"
+            report "ERROR: addr 1 read failed" severity error;
+
+        addr <= "00010"; wait for CLK_PERIOD;
+        assert dout = x"33333333"
+            report "ERROR: addr 2 read failed" severity error;
+
+        addr <= "00011"; wait for CLK_PERIOD;
+        assert dout = x"44444444"
+            report "ERROR: addr 3 read failed" severity error;
+
+        addr <= "00100"; wait for CLK_PERIOD;
+        assert dout = x"55555555"
+            report "ERROR: addr 4 read failed" severity error;
+
+        --------------------------------------------------
+        -- Overwrite address 0 and verify
+        --------------------------------------------------
+        addr <= "00000"; din <= x"AAAAAAAA"; we <= '1';
         wait for CLK_PERIOD;
-
-        assert dout = x"12345678"
-            report "ERROR: RAM read failed"
-            severity error;
-
-        --------------------------------------------------
-        -- overwrite
-        --------------------------------------------------
-        we   <= '1';
-        din  <= x"AAAAAAAA";
-        wait for CLK_PERIOD;
-
         we <= '0';
         wait for CLK_PERIOD;
 
         assert dout = x"AAAAAAAA"
-            report "ERROR: overwrite failed"
-            severity error;
+            report "ERROR: overwrite addr 0 failed" severity error;
 
         report "RAM test passed" severity note;
         wait;
